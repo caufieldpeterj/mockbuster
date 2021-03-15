6416b4c6d210a212332a452cb039e6e055e62a1f
peter caufield <caufield.peterj@gmail.com>
2021-03-15 16:02:01 -0400

    reconciled curl POST request

--- mockbuster_api/controllers/movies.js
+++ mockbuster_api/controllers/movies.js
@@ -1,17 +1,18 @@
 //=======CONFIGURATION=====//
 const express = require('express')
 const MOCKBUSTER = express.Router()
-const Movie = require('../models/movie.js')
+const Movies = require('../models/movie.js')
 
 // curl -X POST -H "Content-Type: application/json" -d '{"title":"GA Week 9"}' 'http://localhost:3003/mockbuster'
 MOCKBUSTER.post('/', (req, res) => {
+    Movies.create(req.body);
     console.log('we received a post request');
     console.log(req.body);
     res.send('we got a post request')
 })
 
-// MOCKBUSTER.get('/', (req, res) => {
-//     res.send(Movie.find(req.body))
-// })
+MOCKBUSTER.get('/', (req, res) => {
+    res.send("here")
+})
 
 module.exports = MOCKBUSTER
