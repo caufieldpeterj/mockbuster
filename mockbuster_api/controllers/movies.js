a21a1b7077fc6a7a497f53253a6656cf23c3ef5b
peter caufield <caufield.peterj@gmail.com>
2021-03-15 16:05:21 -0400

    adjusting curl POST error catching

--- mockbuster_api/controllers/movies.js
+++ mockbuster_api/controllers/movies.js
@@ -4,15 +4,18 @@ const MOCKBUSTER = express.Router()
 const Movies = require('../models/movie.js')
 
 // curl -X POST -H "Content-Type: application/json" -d '{"title":"GA Week 9"}' 'http://localhost:3003/mockbuster'
-MOCKBUSTER.post('/', (req, res) => {
-    Movies.create(req.body);
+MOCKBUSTER.post('/', async (req, res) => {
     console.log('we received a post request');
-    console.log(req.body);
-    res.send('we got a post request')
+    Movie.create(req.body, (error, createdMovie) => {
+      if (error) {
+        res.status(400).json({ error: error.message })
+      }
+      res.status(200).send(createdMovie)
+    })
 })
 
 MOCKBUSTER.get('/', (req, res) => {
-    res.send("here")
+    res.send("Index route");
 })
 
 module.exports = MOCKBUSTER
