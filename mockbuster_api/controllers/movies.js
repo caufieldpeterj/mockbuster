33eee971434800bde8d9ec9a7013d8f6f0459716
RJ G <nazkarrikk@RJs-MacBook-Air.local>
2021-03-16 15:35:48 -0400

    added seed route for testing

--- mockbuster_api/controllers/movies.js
+++ mockbuster_api/controllers/movies.js
@@ -4,6 +4,7 @@ const MOCKBUSTER = express.Router()
 const Movies = require('../models/movie.js')
 
 
+
 //CREATE
 // curl -X POST -H "Content-Type: application/json" -d '{"title":"GA Week 9"}' 'http://localhost:3003/mockbuster'
 MOCKBUSTER.post('/', async (req, res) => {
